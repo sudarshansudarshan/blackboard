db0e4fa40c24401bd04c5071c9a2fda4491ea5c5
Sudarshan Iyengar <sudarshaniyengar@Sudarshans-iMac.local>
2022-04-18 18:44:43 +0530

    kalsdjf

--- sudarshan.py
+++ sudarshan.py
@@ -10,17 +10,18 @@ def disp(m,n):
             print("(",i,j,")",end="")
 
 
-def average_hundred_random():
-    '''Consider 100 random numbers in the range of 1 to 1M and find its average'''
+def average_hundred_random(n):
+    '''Consider n random numbers in the range of 1 to 1M and find its average'''
     L=[]
-    for i in range(100):
-        #append 100 random numbers into L.
+    for i in range(n):
+        #append n random numbers into L.
         L.append(random.randint(1,1000*1000))
     #find the average
     total=0
-    for i in range(100):
+    for i in range(n):
         total=total + L[i]
-    average=total/100
+    average=total/n
+    print(L)
     print("Average is : ",average)
 
 
