781ae6f2f4a7e7fde14461cb59958914a166913e
Sudarshan Iyengar <sudarshaniyengar@Sudarshans-iMac.local>
2022-04-18 18:14:10 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,2 +1,6 @@
 #This is a demo file
 #18th April 2022
+print("Whatever I type here, you will have access to this code")
+# https://github.com/sudarshansudarshan/ge103
+#bookmark this link please. 
+#Let me know what can you see on this link as of now?
